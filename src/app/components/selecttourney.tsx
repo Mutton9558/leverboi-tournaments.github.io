08ec502bc5c54e2121545387accf26f4110d28d7
Mutton9558 <htp8090@gmail.com>
2026-08-15 18:03:11 +0800

    tourney select text too big

--- src/app/components/selecttourney.tsx
+++ src/app/components/selecttourney.tsx
@@ -20,10 +20,10 @@ export default function SelectTourney({imgPath, title, date, path}: SelectTourne
             <div id="tourney-content" className="flex flex-row items-center p-8 w-full">
                 <img src={`images/${imgPath}`} className="w-12 h-12 lg:w-32 lg:h-32 cursor-pointer" alt={`${title}`} fetchPriority="high" />
                 <div className="flex flex-col h-full justify-center ml-16">
-                    <h1 className="font-mono font-semibold text-md lg:text-5xl text-white">{title}</h1>
+                    <h1 className="font-mono font-semibold text-md lg:text-3xl text-white">{title}</h1>
                     <p className="font-mono text-sm lg:text-lg text-white">{date}</p>
                 </div>
-                <p className="absolute right-16 hidden text-5xl lg:flex">&gt;</p>
+                <p className="absolute right-16 hidden text-3xl lg:flex">&gt;</p>
             </div>
             <span className="w-full h-1 bg-white"></span>
         </div>
